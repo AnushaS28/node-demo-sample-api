104ad75f1818f2b75ae3cacdc2d62bbbccf9cd27
praveen3108 <41149045+praveen3108@users.noreply.github.com>
2019-12-23 16:38:42 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 	
 
 // default route
